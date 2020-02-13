9528054352ce08c3909815db46bea128d0536506
Andreas <andrewhoj@gmail.com>
2020-02-13 11:24:25 +0100

    token moved

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const commands = require("./commands");
 
-console.log(process.env.DC_TOKEN);
-
-const token = process.env.DC_TOKEN;
-
 bot.on("ready", () => {
   console.log("Kvantobot online");
 });
 
 bot.on("message", msg => commands(msg));
 
-bot.login(token).catch(e => console.log(e));
+bot.login(process.env.DC_TOKEN).catch(e => console.log(e));
