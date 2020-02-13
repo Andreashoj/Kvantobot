b4a88f599cae9d0c30eb259ccaba135a5fb3fa71
Andreas <andrewhoj@gmail.com>
2020-02-13 11:22:48 +0100

    log

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const commands = require("./commands");
 
+console.log(process.env.DC_TOKEN);
+
 const token = process.env.DC_TOKEN;
 
 bot.on("ready", () => {
