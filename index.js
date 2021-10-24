93b2666a04f6ca621979f41ec25dcc35c327008a
Andreashoj <ah@lait.dk>
2021-10-24 15:52:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const bot = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_ME
 
 const environment = process.env.NODE_ENV;
 
-bot.commands = new Discord.Collection();
+// bot.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./modules/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-  const command = require(`./modules/${file}`);
+// const commandFiles = fs.readdirSync('./modules/').filter(file => file.endsWith('.js'));
+// for (const file of commandFiles) {
+//   const command = require(`./modules/${file}`);
 
-  if (command && command.name) {
-    bot.commands.set(command.name, command);
-  }
-}
+//   if (command && command.name) {
+//     bot.commands.set(command.name, command);
+//   }
+// }
 
 const token =
   environment === 'development'
@@ -30,46 +30,46 @@ bot.on('ready', async () => {
 
 bot.on('message', (msg) => commands(msg));
 
-// Music player
-const prefix = "!"
+// // Music player
+// const prefix = "!"
 
-bot.on('message', msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+// bot.on('message', msg => {
+//   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-  const args = msg.content.slice(prefix.length).split(/ +/);
-  const cmd = args.shift().toLowerCase();
+//   const args = msg.content.slice(prefix.length).split(/ +/);
+//   const cmd = args.shift().toLowerCase();
 
-  const command = bot.commands.get(cmd) || bot.commands.find(a => a.aliases && a.aliases.includes(cmd));
+//   const command = bot.commands.get(cmd) || bot.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
-  try {
-    command.execute(msg, args, cmd, bot, Discord);
-  } catch (err) {
-    msg.reply("There was an error")
-    console.log(err)
-  }
-})
+//   try {
+//     command.execute(msg, args, cmd, bot, Discord);
+//   } catch (err) {
+//     msg.reply("There was an error")
+//     console.log(err)
+//   }
+// })
 
-// CRON jobs
+// // CRON jobs
 
-// Scheduled gaming forecast
-cron.schedule('00 00 9 * * *', () => {
-  randomForecast = parseInt(
-    Math.random() * Math.floor(data.gamingForecast.length)
-  );
-  const channel = bot.channels.get('228187454182522881');
-  channel.send(
-    `Dagens gaming horoskop lyder således..\n **${data.gamingForecast[randomForecast]}**`
-  );
-});
+// // Scheduled gaming forecast
+// cron.schedule('00 00 9 * * *', () => {
+//   randomForecast = parseInt(
+//     Math.random() * Math.floor(data.gamingForecast.length)
+//   );
+//   const channel = bot.channels.get('228187454182522881');
+//   channel.send(
+//     `Dagens gaming horoskop lyder således..\n **${data.gamingForecast[randomForecast]}**`
+//   );
+// });
 
-cron.schedule('00 00 07 * * *', () => {
-  const channel = bot.channels.get('687204415563628602');
+// cron.schedule('00 00 07 * * *', () => {
+//   const channel = bot.channels.get('687204415563628602');
 
-  channel.send(`Godmorgen kvanto, lad os se hvad dagens meloner byder på`);
+//   channel.send(`Godmorgen kvanto, lad os se hvad dagens meloner byder på`);
 
-  setTimeout(() => {
-    channel.send(`!image boobs`);
-  }, 3000);
-});
+//   setTimeout(() => {
+//     channel.send(`!image boobs`);
+//   }, 3000);
+// });
 
-bot.login(token).catch((e) => console.log(e));
+// bot.login(token).catch((e) => console.log(e));
