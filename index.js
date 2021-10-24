fdc54bc20796b5253d12fbb99894de4d83e73079
Andreashoj <ah@lait.dk>
2021-10-24 17:23:52 +0200

    added back music player

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const bot = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_ME
 
 const environment = process.env.NODE_ENV;
 
-// bot.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 
-// const commandFiles = fs.readdirSync('./modules/').filter(file => file.endsWith('.js'));
-// for (const file of commandFiles) {
-//   const command = require(`./modules/${file}`);
+const commandFiles = fs.readdirSync('./modules/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./modules/${file}`);
 
-//   if (command && command.name) {
-//     bot.commands.set(command.name, command);
-//   }
-// }
+  if (command && command.name) {
+    bot.commands.set(command.name, command);
+  }
+}
 
 const token =
   environment === 'development'
@@ -31,45 +31,54 @@ bot.on('ready', async () => {
 bot.on('message', (msg) => commands(msg));
 
 // // Music player
-// const prefix = "!"
+const prefix = "!"
 
-// bot.on('message', msg => {
-//   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+bot.on('message', msg => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-//   const args = msg.content.slice(prefix.length).split(/ +/);
-//   const cmd = args.shift().toLowerCase();
+  const acceptedArgs = ['skip', 'play', 'dj', 'stop']
+  const hasArgs = acceptedArgs.some(word => msg.content.includes(word))
 
-//   const command = bot.commands.get(cmd) || bot.commands.find(a => a.aliases && a.aliases.includes(cmd));
+  if (!hasArgs) {
+    return;
+  }
 
-//   try {
-//     command.execute(msg, args, cmd, bot, Discord);
-//   } catch (err) {
-//     msg.reply("There was an error")
-//     console.log(err)
-//   }
-// })
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
+
+  const command = bot.commands.get(cmd) || bot.commands.find(a => a.aliases && a.aliases.includes(cmd));
+
+  console.log(args, hasArgs)
+
+  try {
+    command.execute(msg, args, cmd, bot, Discord);
+  } catch (err) {
+    msg.reply("There was an error")
+    console.log(err)
+  }
+})
 
 // // CRON jobs
 
 // // Scheduled gaming forecast
-// cron.schedule('00 00 9 * * *', () => {
-//   randomForecast = parseInt(
-//     Math.random() * Math.floor(data.gamingForecast.length)
-//   );
-//   const channel = bot.channels.get('228187454182522881');
-//   channel.send(
-//     `Dagens gaming horoskop lyder således..\n **${data.gamingForecast[randomForecast]}**`
-//   );
-// });
+cron.schedule('00 00 9 * * *', () => {
+  randomForecast = parseInt(
+    Math.random() * Math.floor(data.gamingForecast.length)
+  );
+  const channel = bot.channels.get('228187454182522881');
+  channel.send(
+    `Dagens gaming horoskop lyder således..\n **${data.gamingForecast[randomForecast]}**`
+  );
+});
 
-// cron.schedule('00 00 07 * * *', () => {
-//   const channel = bot.channels.get('687204415563628602');
+cron.schedule('00 00 07 * * *', () => {
+  const channel = bot.channels.get('687204415563628602');
 
-//   channel.send(`Godmorgen kvanto, lad os se hvad dagens meloner byder på`);
+  channel.send(`Godmorgen kvanto, lad os se hvad dagens meloner byder på`);
 
-//   setTimeout(() => {
-//     channel.send(`!image boobs`);
-//   }, 3000);
-// });
+  setTimeout(() => {
+    channel.send(`!image boobs`);
+  }, 3000);
+});
 
 bot.login(token).catch((e) => console.log(e));
--- modules/play.js
+++ modules/play.js
@@ -1,138 +1,138 @@
-// const ytdl = require('ytdl-core');
-// const ytSearch = require('yt-search');
-// const queue = new Map();
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+const queue = new Map();
 
-// const brandtDenial = '<:brandt_cool:419955344274489355> Glem det fyr';
-// const brandtJAMMER = '<:brandt_cool:419955344274489355> PARTYYYYY';
-// const anzBANGER = '<:supersayian_anz:422878195306594325> BANGER ALERT';
-// const komoGAY = '<:kevin_gay:419964305719689216> Den går lige i pøllen';
-// const brunoJAMS = '<:omega_bruno:426832042160947211> nice.';
-// const nickuJAMS = '<:nicklas_hype:419958177501675540> HYYYPE';
-// const brandtDAMER = '<:brandt_damer:419964006913277962> om i køen';
-// const reactions = [anzBANGER, komoGAY, brunoJAMS, brandtJAMMER, nickuJAMS];
+const brandtDenial = '<:brandt_cool:419955344274489355> Glem det fyr';
+const brandtJAMMER = '<:brandt_cool:419955344274489355> PARTYYYYY';
+const anzBANGER = '<:supersayian_anz:422878195306594325> BANGER ALERT';
+const komoGAY = '<:kevin_gay:419964305719689216> Den går lige i pøllen';
+const brunoJAMS = '<:omega_bruno:426832042160947211> nice.';
+const nickuJAMS = '<:nicklas_hype:419958177501675540> HYYYPE';
+const brandtDAMER = '<:brandt_damer:419964006913277962> om i køen';
+const reactions = [anzBANGER, komoGAY, brunoJAMS, brandtJAMMER, nickuJAMS];
 
-// function randomNum(min, max) {
-//   return Math.round(Math.random() * (max - min + 1) + min);
-// }
+function randomNum(min, max) {
+  return Math.round(Math.random() * (max - min + 1) + min);
+}
 
-// const getSong = async (message, args, server_queue, voice_channel) => {
-//   if (!args.length)
-//     return message.channel.send('You need to send the second argument!');
-//   let song = {};
+const getSong = async (message, args, server_queue, voice_channel) => {
+  if (!args.length)
+    return message.channel.send(brandtDenial);
+  let song = {};
 
-//   //If the first argument is a link. Set the song object to have two keys. Title and URl.
-//   if (ytdl.validateURL(args[0])) {
-//     const song_info = await ytdl.getInfo(args[0]);
-//     song = {
-//       title: song_info.videoDetails.title,
-//       url: song_info.videoDetails.video_url,
-//     };
-//   } else {
-//     //If there was no link, we use keywords to search for a video. Set the song object to have two keys. Title and URl.
-//     const video_finder = async (query) => {
-//       const video_result = await ytSearch(query);
-//       return video_result.videos.length > 1 ? video_result.videos[0] : null;
-//     };
+  //If the first argument is a link. Set the song object to have two keys. Title and URl.
+  if (ytdl.validateURL(args[0])) {
+    const song_info = await ytdl.getInfo(args[0]);
+    song = {
+      title: song_info.videoDetails.title,
+      url: song_info.videoDetails.video_url,
+    };
+  } else {
+    //If there was no link, we use keywords to search for a video. Set the song object to have two keys. Title and URl.
+    const video_finder = async (query) => {
+      const video_result = await ytSearch(query);
+      return video_result.videos.length > 1 ? video_result.videos[0] : null;
+    };
 
-//     const video = await video_finder(args.join(' '));
-//     if (video) {
-//       song = { title: video.title, url: video.url };
-//     } else {
-//       message.channel.send('<:pepehands:538114405838225409> ingen video');
-//     }
-//   }
+    const video = await video_finder(args.join(' '));
+    if (video) {
+      song = { title: video.title, url: video.url };
+    } else {
+      message.channel.send('<:pepehands:538114405838225409> ingen video');
+    }
+  }
 
-//   //If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
-//   if (!server_queue) {
-//     const queue_constructor = {
-//       voice_channel: voice_channel,
-//       text_channel: message.channel,
-//       connection: null,
-//       songs: [],
-//     };
+  //If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
+  if (!server_queue) {
+    const queue_constructor = {
+      voice_channel: voice_channel,
+      text_channel: message.channel,
+      connection: null,
+      songs: [],
+    };
 
-//     //Add our key and value pair into the global queue. We then use this to get our server queue.
-//     queue.set(message.guild.id, queue_constructor);
-//     queue_constructor.songs.push(song);
+    //Add our key and value pair into the global queue. We then use this to get our server queue.
+    queue.set(message.guild.id, queue_constructor);
+    queue_constructor.songs.push(song);
 
-//     //Establish a connection and play the song with the vide_player function.
-//     try {
-//       const connection = await voice_channel.join();
-//       queue_constructor.connection = connection;
-//       video_player(message.guild, queue_constructor.songs[0]);
-//     } catch (err) {
-//       queue.delete(message.guild.id);
-//       message.channel.send(':robot: FEJL I SYSTEMET');
-//       throw err;
-//     }
-//   } else {
-//     server_queue.songs.push(song);
-//     return message.channel.send(`${brandtDAMER} om i køen - **${song.title}**`);
-//   }
-// };
+    //Establish a connection and play the song with the vide_player function.
+    try {
+      const connection = await voice_channel.join();
+      queue_constructor.connection = connection;
+      video_player(message.guild, queue_constructor.songs[0]);
+    } catch (err) {
+      queue.delete(message.guild.id);
+      message.channel.send(':robot: FEJL I SYSTEMET');
+      throw err;
+    }
+  } else {
+    server_queue.songs.push(song);
+    return message.channel.send(`${brandtDAMER} om i køen - **${song.title}**`);
+  }
+};
 
-// module.exports = {
-//   name: 'play',
-//   aliases: ['skip', 'stop', 'dj'], //We are using aliases to run the skip and stop command follow this tutorial if lost: https://www.youtube.com/watch?v=QBUJ3cdofqc
-//   cooldown: 0,
-//   description: 'Advanced music bot',
-//   async execute(message, args, cmd, client, Discord) {
-//     //Checking for the voicechannel and permissions (you can add more permissions if you like).
-//     const voice_channel = message.member.voice.channel;
-//     if (!voice_channel) return message.channel.send(brandtDenial);
-//     const permissions = voice_channel.permissionsFor(message.client.user);
-//     if (!permissions.has('CONNECT')) return message.channel.send(brandtDenial);
-//     if (!permissions.has('SPEAK')) return message.channel.send(brandtDenial);
+module.exports = {
+  name: 'play',
+  aliases: ['skip', 'stop', 'dj'], //We are using aliases to run the skip and stop command follow this tutorial if lost: https://www.youtube.com/watch?v=QBUJ3cdofqc
+  cooldown: 0,
+  description: 'Advanced music bot',
+  async execute(message, args, cmd, client, Discord) {
+    //Checking for the voicechannel and permissions (you can add more permissions if you like).
+    const voice_channel = message.member.voice.channel;
+    if (!voice_channel) return message.channel.send(brandtDenial);
+    const permissions = voice_channel.permissionsFor(message.client.user);
+    if (!permissions.has('CONNECT')) return message.channel.send(brandtDenial);
+    if (!permissions.has('SPEAK')) return message.channel.send(brandtDenial);
 
-//     //This is our server queue. We are getting this server queue from the global queue.
-//     const server_queue = queue.get(message.guild.id);
+    //This is our server queue. We are getting this server queue from the global queue.
+    const server_queue = queue.get(message.guild.id);
 
-//     //If the user has used the play command
-//     if (cmd === 'play') {
-//       await getSong(message, args, server_queue, voice_channel);
-//     } else if (cmd === 'dj') {
-//       await getSong(message, ['det gutterne'], server_queue, voice_channel);
-//       return message.channel.send(
-//         'https://tenor.com/view/dancing-stanley-li-dj-ravine-jamming-dj-gif-17539848'
-//       );
-//     } else if (cmd === 'skip') skip_song(message, server_queue);
-//     else if (cmd === 'stop') stop_song(message, server_queue);
-//   },
-// };
+    //If the user has used the play command
+    if (cmd === 'play') {
+      await getSong(message, args, server_queue, voice_channel);
+    } else if (cmd === 'dj') {
+      await getSong(message, ['det gutterne'], server_queue, voice_channel);
+      return message.channel.send(
+        'https://tenor.com/view/dancing-stanley-li-dj-ravine-jamming-dj-gif-17539848'
+      );
+    } else if (cmd === 'skip') skip_song(message, server_queue);
+    else if (cmd === 'stop') stop_song(message, server_queue);
+  },
+};
 
-// const video_player = async (guild, song) => {
-//   const song_queue = queue.get(guild.id);
+const video_player = async (guild, song) => {
+  const song_queue = queue.get(guild.id);
 
-//   //If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
-//   if (!song) {
-//     song_queue.voice_channel.leave();
-//     queue.delete(guild.id);
-//     return;
-//   }
-//   const stream = ytdl(song.url, { filter: 'audioonly' });
-//   song_queue.connection
-//     .play(stream, { seek: 0, volume: 0.5 })
-//     .on('finish', () => {
-//       song_queue.songs.shift();
-//       video_player(guild, song_queue.songs[0]);
-//     });
-//   await song_queue.text_channel.send(
-//     `${reactions[randomNum(0, reactions.length)]} - **${song.title}**`
-//   );
-// };
+  //If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
+  if (!song) {
+    song_queue.voice_channel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+  const stream = ytdl(song.url, { filter: 'audioonly' });
+  song_queue.connection
+    .play(stream, { seek: 0, volume: 0.5 })
+    .on('finish', () => {
+      song_queue.songs.shift();
+      video_player(guild, song_queue.songs[0]);
+    });
+  await song_queue.text_channel.send(
+    `${reactions[randomNum(0, reactions.length)]} - **${song.title}**`
+  );
+};
 
-// const skip_song = (message, server_queue) => {
-//   if (!message.member.voice.channel) return message.channel.send(brandtDenial);
-//   if (!server_queue) {
-//     return message.channel.send(
-//       `Ikke flere bangers <:pepehands:538114405838225409>`
-//     );
-//   }
-//   server_queue.connection.dispatcher.end();
-// };
+const skip_song = (message, server_queue) => {
+  if (!message.member.voice.channel) return message.channel.send(brandtDenial);
+  if (!server_queue) {
+    return message.channel.send(
+      `Ikke flere bangers <:pepehands:538114405838225409>`
+    );
+  }
+  server_queue.connection.dispatcher.end();
+};
 
-// const stop_song = (message, server_queue) => {
-//   if (!message.member.voice.channel) return message.channel.send(brandtDenial);
-//   server_queue.songs = [];
-//   server_queue.connection.dispatcher.end();
-// };
+const stop_song = (message, server_queue) => {
+  if (!message.member.voice.channel) return message.channel.send(brandtDenial);
+  server_queue.songs = [];
+  server_queue.connection.dispatcher.end();
+};
